37870890ebeb017350789abb81d67ef2b0c08e73
zeb32ra <zeb32ra@gmail.com>
2023-03-10 22:22:06 +0300

    Almost done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,147 @@ namespace MusicPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] files;
+        List<string> filesList_for_media = new List<string>();
+        List<string> filesList_for_listbox = new List<string>();
+        int selected_index = 0;
+        // flag for choosing again
+        bool play = true;
+        bool play_again= false;
+        bool mixing= false;
+        Random random = new Random();
+        List<string> copy = new List<string>();
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Papka_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog { IsFolderPicker=true};
+            var result = dialog.ShowDialog();
+            if (result == CommonFileDialogResult.Ok)
+            {
+               files = Directory.GetFiles(dialog.FileName);
+            }
+            foreach(string file in files)
+            {
+                if (file.EndsWith(".mp3") || file.EndsWith(".m4a") || file.EndsWith(".flac"))
+                {
+                    filesList_for_listbox.Add(file.Substring(file.LastIndexOf("\\") + 1, file.LastIndexOf('.') - file.LastIndexOf("\\")));
+                    filesList_for_media.Add(file);
+                    copy.Add(file);
+                }
+            }
+            if(filesList_for_listbox.Count > 0 & filesList_for_media.Count > 0)
+            {
+                selected_index = 0;
+
+                List.ItemsSource = filesList_for_listbox;
+                Volume_slide.Value = 70;
+                Volume_slide.Maximum = 100;
+                media.Source = new Uri(filesList_for_media[selected_index]);
+                List.SelectedIndex = selected_index;
+                media.Volume = Volume_slide.Value / 100;
+                media.Play();
+                // thread start
+            }
+        }
+
+        private void Length_slide_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void Previous_Click(object sender, RoutedEventArgs e)
+        {
+            if (play_again)
+            {
+                media.Source = new Uri(filesList_for_media[selected_index]);
+            }
+            else
+            {
+                if (selected_index == 0)
+                {
+                    selected_index = filesList_for_media.Count;
+                }
+                media.Source = new Uri(filesList_for_media[selected_index - 1]);
+                List.SelectedIndex = copy.IndexOf(filesList_for_media[selected_index - 1]);
+                selected_index -= 1;
+            }
+        }
+
+        private void Play_Pause_Click(object sender, RoutedEventArgs e)
+        {
+            if (play)
+            {
+                play = false;
+                media.Pause();
+            }
+            else
+            {
+                play = true;
+                media.Play();
+            }
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            if (play_again)
+            {
+                media.Source = new Uri(filesList_for_media[selected_index]);
+            }
+            else
+            {
+                if (selected_index == filesList_for_media.Count - 1)
+                {
+                    selected_index = -1;
+                }
+                media.Source = new Uri(filesList_for_media[selected_index + 1]);
+                List.SelectedIndex = copy.IndexOf(filesList_for_media[selected_index + 1]);
+                selected_index += 1;
+            }
+            
+        }
+
+        private void Again_Click(object sender, RoutedEventArgs e)
+        {
+            if (play_again)
+            {
+                play_again= false;
+            }
+            else
+            {
+                play_again = true;
+            }
+        }
+
+        private void Mix_Click(object sender, RoutedEventArgs e)
+        {
+            if (mixing)
+            {
+                mixing = false;
+                filesList_for_media.Clear();
+                filesList_for_media.AddRange(copy);
+                selected_index = -1;
+            }
+            else
+            {
+                mixing = true;
+                for(int i = filesList_for_media.Count - 1; i>=1; i--)
+                {
+                    int j = random.Next(i + 1);
+                    var t = filesList_for_media[j];
+                    filesList_for_media[j] = filesList_for_media[i];
+                    filesList_for_media[i] = t;
+                }
+            }
+        }
+
+        private void Volume_slide_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            media.Volume = Volume_slide.Value / 100;
+        }
+
     }
 }
